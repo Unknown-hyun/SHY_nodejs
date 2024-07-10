66a408d1bfcc83457c97446a04568785dcf37a84
unknown-hyun <xxxhyun88@gmail.com>
2024-07-10 15:07:20 +0900

    20240710

--- n15.js
+++ n15.js
@@ -9,9 +9,10 @@ const _path = path.join(__dirname, "/");
 app.use(logger("tiny"));
 
 app.get("/", (req, res) => {
+  const name = "save";
   const data = "파일내용이 작성 됨";
   res.send("파일이 저장되었습니다.");
-  fs.writeFile(_path + "/save.text", data, (e) => {
+  fs.writeFile(_path + name + ".text", data, (e) => {
     if (e) console.log(e); // 에러시 에러 내용 출력
     console.log("파일작성이 완료되었습니다.");
   });
