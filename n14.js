ae08f49fb97311320302bad6573689fffe3de60e
unknown-hyun <xxxhyun88@gmail.com>
2024-07-10 11:49:34 +0900

    n14수정중

--- n14.js
+++ n14.js
@@ -1,1 +1,19 @@
-// 안녕하세요
+const express = require("express");
+const logger = require("morgan");
+const fs = require("fs");
+const app = express();
+const port = 3000;
+
+app.get("/list", (req, res) => {
+  fs.readdir(__dirname, "utf-8", (err, data) => {
+    res.send(data);
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Funkin T");
+});
+
+app.listen(port, (req, res) => {
+  console.log(port + " 서버");
+});
