1b07770bdeed38a5aa537fad20ca217938e110e3
unknown-hyun <xxxhyun88@gmail.com>
2024-07-17 09:20:43 +0900

    완료

--- web2/server.js
+++ web2/server.js
@@ -26,6 +26,72 @@ app.get("/", (req, res) => {
   console.log("웹에 정상 접속하였습니다");
 });
 
+app.get("/list", (req, res) => {
+  db.query("SELECT * FROM web2", (err, results) => {
+    const data = results;
+    console.log(data);
+
+    let list = `    <!DOCTYPE html>`;
+    list += `<html lang="ko">`;
+    list += `  <head>`;
+    list += `    <meta charset="UTF-8" />`;
+    list += `    <meta name="viewport" content="width=device-width, initial-scale=1.0" />`;
+    list += `    <title>리스트</title>`;
+    list += `    <style>`;
+    list += `      table {`;
+    list += `        border-collapse: collapse;`;
+    list += `        text-align: center;`;
+    list += `        width: 80%;`;
+    list += `        margin: auto;`;
+    list += `      }`;
+    list += ``;
+    list += `      th,`;
+    list += `      tr,`;
+    list += `      td {`;
+    list += `        border: 1px solid #ccc;`;
+    list += `      }`;
+    list += ``;
+    list += `      td {`;
+    list += `        width: 20%;`;
+    list += `      }`;
+    list += ``;
+    list += `      h2 {`;
+    list += `        text-align: center;`;
+    list += `      }`;
+    list += ``;
+    list += `      th {`;
+    list += `        background-color: lightblue;`;
+    list += `      }`;
+    list += `    </style>`;
+    list += `  </head>`;
+    list += ``;
+    list += `  <body>`;
+    list += `    <h2>Database List</h2>`;
+    list += `    <table>`;
+    list += `      <tr>`;
+    list += `        <th>No.</th>`;
+    list += `        <th>ID</th>`;
+    list += `        <th>Password</th>`;
+    list += `        <th>Name</th>`;
+    list += `        <th>E-Mail</th>`;
+    list += `      </tr>`;
+    data.forEach((v) => {
+      list += `      <tr>`;
+      list += `        <td>${v.Number}</td>`;
+      list += `        <td>${v.ID}</td>`;
+      list += `        <td>${v.Password}</td>`;
+      list += `        <td>${v.Name}</td>`;
+      list += `        <td>${v.email}</td>`;
+      list += `      </tr>`;
+    });
+    list += `<button type="button" onclick="location.href='/'">Back</button>`;
+    list += `    </table>`;
+    list += `  </body>`;
+    list += `</html>`;
+    res.send(list);
+  });
+});
+
 app.get("/data", (req, res) => {
   const { ID, Password, Name, email } = req.query;
   db.query(
